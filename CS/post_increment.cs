03e702fe909ab0e9333d513511c6c2b183bdec36
payal281 <payalkutemate29@gmail.com>
2023-10-13 21:49:07 +0530

    cs

---
+++ CS/post_increment.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace program
+{
+    class Program
+    {
+        static void Main()
+        {
+            int i = 0;
+            int j = i++;
+            Console.WriteLine("value of i: " + i +" "+ "value of j: " + j);
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ CS/print_greatest_no.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace print__greatest_no
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
