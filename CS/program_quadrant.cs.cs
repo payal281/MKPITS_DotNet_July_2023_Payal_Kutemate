0ef659a0630b8d41ea28ea8eea7618f151bc8a83
payal281 <payalkutemate29@gmail.com>
2023-10-16 17:19:29 +0530

    cs

---
+++ CS/program_quadrant.cs.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace program_quadrant
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x, y;
+            Console.WriteLine("enter x value");
+            x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("enter y value");
+            y = Convert.ToInt32(Console.ReadLine());
+
+            if (x > 0 && y > 0)
+            {
+                Console.WriteLine("The coordinate point" + (x, y) + "lies in the 1st quadrant");
+            }
+            else if (x > 0 && y < 0)
+            {
+                Console.WriteLine("The coordinate point" + (x, y) + "lies in the 2st quadrant");
+
+            }
+            else if (x < 0 && y < 0)
+            {
+                Console.WriteLine("The coordinate point " + (x, y) + "lies in the 3rd quadrant");
+
+            }
+            else if(x<0 && y>0)
+            {
+                Console.WriteLine("The coordinate point " + (x, y) + "lies in the 4th quadrant");
+
+            }
+            {
+                Console.ReadKey();
+            }        
+        }
+    }
+}7
---
+++ CS/program_to_readvalue.cs.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace program_to_readvalue
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int m;
+            Console.WriteLine("enter number: ");
+            m = Convert.ToInt32(Console.ReadLine());
+
+            if (m > 0)
+                Console.WriteLine("n=1");
+            else
+                Console.WriteLine("n=-1");
+            Console.ReadKey();
+
+
+        }
+    }
+}
