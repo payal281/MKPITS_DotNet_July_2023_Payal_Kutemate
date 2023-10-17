4744d2c696633d6ce028a21a9f2265c32c970daf
payal281 <payalkutemate29@gmail.com>
2023-10-17 17:03:09 +0530

    cs

---
+++ CS/Print_dayno.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace print_dayno
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int dayno;
+            Console.WriteLine("enter dayno between 1 and 7");
+            dayno = Convert.ToInt32(Console.ReadLine());
+            switch (dayno)
+            {
+                case 1:
+                    Console.WriteLine("monday");
+                    break;
+
+                case 2:
+                    Console.WriteLine("tuesday");
+                    break;
+
+                default:
+                    Console.WriteLine("some other day");
+                    break;
+            }
+            Console.ReadKey();
+        }
+    }
+}
+
+
---
+++ CS/designation.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace designation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string designation;
+            int bonus = 0;
+            Console.WriteLine("enter designation manager,clerk or peon");
+            designation = Console.ReadLine();
+            switch (designation)
+            {
+                case "manager":
+                    bonus = 10000;
+                    break;
+
+                case "clerk":
+                    bonus = 5000; ;
+                    break;
+                case "peon":
+                    bonus = 2000; ;
+                    break;
+
+                default:
+                    Console.WriteLine("invalid designation");
+                    break;
+            }
+
+            Console.WriteLine("bonus = {0}", bonus);
+            Console.ReadKey();
+        }
+    }
+}
+
