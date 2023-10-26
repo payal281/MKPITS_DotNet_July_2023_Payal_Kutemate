57193179c3c316be7d6eee69e784b11a41359529
payal281 <payalkutemate29@gmail.com>
2023-10-26 18:19:53 +0530

    cs

---
+++ CS/sum_no_in_array.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sum_number_in_array
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] num = new int[3];
+            for (int cnt = 0; cnt < 3; cnt++)
+            {
+                Console.WriteLine("enter number");
+                num[cnt] = Convert.ToInt32(Console.ReadLine());
+            }
+            for (int cnt = 0; cnt < 3; cnt++)
+            {
+                Console.WriteLine("num[{0}]= {1}", cnt, num[cnt]);
+
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ CS/variable_length_parameter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace variable_length_parameter
+{
+    internal class Program
+    {
+        static int calculate(params int[]num)
+        {
+            int cnt, total = 0;
+            for (cnt = 0; cnt < num.Length; cnt++)
+            {
+                total = total + num[cnt];//0+1=1,1+2=3, 3+3=6
+            }
+            return total;
+        }
+        static void Main(string[] args)
+        {
+            int result = calculate(1, 2, 3);
+            Console.WriteLine("total" + result);
+            Console.ReadLine();
+        }
+    }
+}
