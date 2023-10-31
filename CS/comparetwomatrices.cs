eb569cd9f11f53606da087729be85f8bece0ae05
payal281 <payalkutemate29@gmail.com>
2023-10-31 18:57:38 +0530

    cs

---
+++ CS/comparetwomatrices.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] a1 = new int[2, 2];
+            int[,] a2 = new int[2, 2];
+            for (int cnt1 = 0; cnt1 < 2; cnt1++)
+            {
+                for (int cnt2 = 0; cnt2 < 2; cnt2++)
+                {
+                    Console.Write("Enter element of array 1 :");
+                    a1[cnt1, cnt2] = Convert.ToInt32(Console.ReadLine());
+                }
+            }
+            for (int cnt1 = 0; cnt1 < 2; cnt1++)
+            {
+                for (int cnt2 = 0; cnt2 < 2; cnt2++)
+                {
+                    Console.Write("Enter element of array 2 :");
+                    a2[cnt1, cnt2] = Convert.ToInt32(Console.ReadLine());
+                }
+            }
+            int flag = 0;
+            for (int cnt1 = 0; cnt1 < 2; cnt1++)
+            {
+                for (int cnt2 = 0; cnt2 < 2; cnt2++)
+                {
+                    if (a1[cnt1, cnt2] != a2[cnt1, cnt2])
+                    {
+                        flag = 1;
+                        break;
+                    }
+                }
+            }
+            Console.WriteLine("The first matrix is : ");
+            for (int cnt1 = 0; cnt1 < 2; cnt1++)
+            {
+                for (int cnt2 = 0; cnt2 < 2; cnt2++)
+                {
+
+                    Console.Write(a1[cnt1, cnt2] + "\t");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine("The second matrix is :");
+            for (int cnt1 = 0; cnt1 < 2; cnt1++)
+            {
+                for (int cnt2 = 0; cnt2 < 2; cnt2++)
+                {
+
+                    Console.Write(a2[cnt1, cnt2] + "\t");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine("The Matrices can be compared :");
+            if (flag == 0)
+            {
+                Console.WriteLine("Two matrices are equal");
+            }
+            else
+            {
+                Console.WriteLine("Two matrices are not equal");
+            }
+            Console.ReadKey();
+        }
+
+    }
+}
---
+++ CS/multiplictionoftwomatrices.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            int[,] a1 = new int[2, 2];
+            int[,] a2 = new int[2, 2];
+            int[,] a3 = new int[2, 2];
+            for (int count1 = 0; count1 < 2; count1++)
+            {
+                for (int count2 = 0; count2 < 2; count2++)
+                {
+                    Console.Write("Enter [{0}][{1}] element of array 1 : ", count1, count2);
+                    a1[count1, count2] = Convert.ToInt32(Console.ReadLine());
+                }
+            }
+            for (int count1 = 0; count1 < 2; count1++)
+            {
+                for (int count2 = 0; count2 < 2; count2++)
+                {
+                    Console.Write("Enter [{0}][{1}] element of array 2 : ", count1, count2);
+                    a2[count1, count2] = Convert.ToInt32(Console.ReadLine());
+                }
+            }
+            for (int count1 = 0; count1 < 2; count1++)
+            {
+                for (int count2 = 0; count2 < 2; count2++)
+                {
+                    a3[count1, count2] = a1[count1, count2] * a2[count1, count2];
+                }
+            }
+            for (int count1 = 0; count1 < 2; count1++)
+            {
+                for (int count2 = 0; count2 < 2; count2++)
+                {
+                    Console.Write(a3[count1, count2] + "\t");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+
+    }
+}
