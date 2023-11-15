26589c4610b9fbfa0f1193787e84b8339c776d89
payal281 <payalkutemate29@gmail.com>
2023-11-15 19:47:23 +0530

    cs

---
+++ CS/static_ex.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp15
+{
+    class Book
+    {
+        public static int bookid;
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Book.bookid = 12;
+            Console.WriteLine("book id " + Book.bookid);
+            Console.ReadLine();
+        }
+    }
+}
