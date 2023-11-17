dd8aa692c8906dffe2ffdaac7810df45e382a4de
payal281 <payalkutemate29@gmail.com>
2023-11-17 16:04:53 +0530

    cs

---
+++ CS/RuntimePolymorphiswithDataMembers.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+public class Animal
+{
+    public string color = "white";
+
+}
+public class Dog : Animal
+{
+    public string color = "black";
+}
+public class TestSealed
+{
+    public static void Main()
+    {
+        Animal d = new Dog();
+        Console.WriteLine(d.color);
+        Console.ReadKey();
+
+    }
+}
