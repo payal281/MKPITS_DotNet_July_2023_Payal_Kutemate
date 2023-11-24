c7fe6716a5e5db92d4f773b8de95e905dd73930a
payal281 <payalkutemate29@gmail.com>
2023-11-24 18:08:02 +0530

    cs

---
+++ CS/Sortedlist_.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace ConsoleApp66
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SortedList sl= new SortedList();
+            sl.Add("001", "payal");
+            sl.Add("002", "partu");
+            sl.Add("003", "neha");
+            sl.Add("004", "snehal");
+            sl.Add("005", "prachi");
+            sl.Add("006", "prajval");
+            sl.Add("007", "nikita");
+
+            if (sl.ContainsValue("pranita"))
+            {
+                Console.WriteLine("this student name already in the list");
+
+            }
+            else
+            {
+                sl.Add("008", "vidhi");
+            }
+            //get the collection of key
+            ICollection key = sl.Keys;
+            foreach(string k in key)
+            {
+                Console.WriteLine(k + ": " + sl[k]);
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ CS/hashtable_store_datainkeyValue.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+using System.Diagnostics.Eventing.Reader;
+
+
+namespace ConsoleApp65
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Hashtable ht = new Hashtable();
+            ht.Add("001", "prachi");
+            ht.Add("002", "pratu");
+            ht.Add("003", "neha");
+            ht.Add("004", "snehal");
+            ht.Add("005", "vidhi");
+            ht.Add("006", "priti");
+            ht.Add("007", "nikita");
+            if (ht.ContainsValue("payal"))
+            {
+                Console.WriteLine("The student already in the list");
+            }
+            else
+            {
+                ht.Add("008", "payal");
+            }
+            //get collection of the keys.\
+            ICollection key=ht.Keys;
+            foreach (string k in key)
+            {
+                Console.WriteLine(k + ":" + ht[k]);
+
+            }
+            Console.ReadKey();
+        
+
+        }
+    }
+}
+
