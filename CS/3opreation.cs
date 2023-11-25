514622ded5bec7fece101879ed79893458c55532
payal281 <payalkutemate29@gmail.com>
2023-11-25 21:56:05 +0530

    cs

--- CS/3opreation.cs
+++ CS/3opreation.cs
@@ -1,22 +1,47 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace _3opreation
 {
-    internal static class Program
+    public partial class Form1 : Form
     {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
-        [STAThread]
-        static void Main()
+        public Form1()
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int num1 = Convert.ToInt32(textBox1.Text);
+            int num2 = Convert.ToInt32(textBox2.Text);
+            int result = (num1 + num2);
+            label3.Text = "addition :" + result;
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int num1 = Convert.ToInt32(textBox1.Text);
+            int num2 = Convert.ToInt32(textBox2.Text);
+            int result = num1 - num2;
+            label3.Text = "substract" + result;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int num1 = Convert.ToInt32(textBox1.Text);
+            int num2 = Convert.ToInt32(textBox2.Text);
+            int result = num1 * num2;
+            label3.Text = "multiply" + result;
+
         }
     }
 }
---
+++ CS/Queue.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp68
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue q = new Queue();
+            q.Enqueue("A");
+            q.Enqueue("M");
+            q.Enqueue("G");
+            q.Enqueue("W");
+            Console.WriteLine("Current queue:");
+            foreach(char c in q) Console.Write(c+"");
+
+            Console.WriteLine();
+            q.Enqueue("V");
+            q.Enqueue("H");
+            Console.WriteLine("current queue:");
+            foreach(char c in q) Console.Write(c+"");
+
+            Console.WriteLine();
+            Console.WriteLine("removing same value");
+            char ch=(char)q.Dequeue();
+            Console.WriteLine("The removed value:{0}", ch);
+            ch=(char)q.Dequeue();
+            Console.WriteLine("The removed value:{0}", ch);
+            Console.ReadKey();
+           
+        }
+    }
+}
