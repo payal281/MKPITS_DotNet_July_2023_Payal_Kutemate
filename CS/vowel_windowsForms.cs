a317e43814cf017b71946d553443f734d8f0c93a
payal281 <payalkutemate29@gmail.com>
2023-12-02 23:08:55 +0530

    cs

---
+++ CS/vowel_windowsForms.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp28
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            char ch;
+            ch = Convert.ToChar(textBox1.Text);
+
+            switch(char.ToLower(ch))
+            {
+                case 'a':
+                    label2.Text = "vowel";
+                    break;
+                case 'e':
+                    label2.Text = "vowel";
+                    break;
+                case 'i':
+                    label2.Text = "vowel";
+                    break;
+                case 'o':
+                    label2.Text = "vowel";
+                    break;
+                case 'u':
+                    label2.Text = "vowel";
+                    break;
+                    
+                      default:
+                    label2.Text = " not a vowel";
+                       break;
+          
+            }        
+                    
+        }
+    }
+}
---
+++ CS/weekday_windowsforms.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp29
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int day = 7;
+            day=Convert.ToInt32(textBox1.Text);
+            switch(day)
+            {
+                case 1:
+                    label2.Text=("Monday");
+                    break;
+                case 2:
+                    label2.Text = ("Tuesday");
+                    break;
+                case 3:
+                    label2.Text = ("Wednesday");
+                    break;
+                case 4:
+                   label2.Text = ("Thursday");
+                    break;
+                case 5:
+                    label2.Text = ("Friday");
+                    break;
+                case 6:
+                    label2.Text = ("Saturday");
+                    break;
+                case 7:
+                    label2.Text = ("Sunday");
+                    break;
+                default:
+                    label2.Text = ("incorrect");
+                       break;
+            }
+        }
+    }
+}
