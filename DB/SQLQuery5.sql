5d70ae97aa5730e42cfb430e36a391f25a53ff3c
payal281 <payalkutemate29@gmail.com>
2023-12-13 17:44:46 +0530

    dB

---
+++ DB/SQLQuery5.sql
@@ -0,0 +1,6 @@
+create table employee(empid int primary key identity(1,1),empname varchar(20))
+insert into employee(empname) values('ravi')
+insert into employee(empname) values('rajesh')
+insert into employee(empname) values('rajesh123')
+delete from employee
+select * from employee
